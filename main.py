fbf5107eb60bd929f3ac7252238ab46e7600103b
Maxim Ermolaev <ermolaev.me@jhvn.tk>
2022-07-01 15:47:20 +0300

    Добавлена текстовая информация

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from parser import Parser
+import datetime
 
-parser = Parser()
-parser.parse_vacancies()
+if __name__ == '__main__':
+    print('\n\n\tПарсер вакансий школ. Время: ' + str(datetime.datetime.now()))
+    print('\tНачата работа парсера...')
+    init_time = datetime.datetime.now()
+    parser = Parser(filename='vacancies.txt')
+    parser.parse_vacancies()
+    end_time = datetime.datetime.now()
+    print('\tРабота парсера окончена. Данные доступны в файле ' +
+          parser.get_filename() + '\n\tВремя работы парсера: ' + str(end_time - init_time))
